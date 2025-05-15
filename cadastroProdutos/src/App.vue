cb1d75edfa60e4e2d44b2adad7c0c056ad5da285
Gabriel Araujo <gabrielfroesm@gmail.com>
2025-05-15 16:01:26 -0300

    feat(cadastroProdutos) adicionando componente de cadastro

---
+++ cadastroProdutos/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import cadastroProduto from './components/cadastroProduto.vue';
+</script>
+
+<template>
+  <cadastroProduto msg="cadastro Produto"/>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-image: linear-gradient(to bottom right, #3f3f3f, #6c6a6a);
+  background-attachment: fixed;
+  min-height: 100vh;
+  color: #f0f0f0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+}
+</style>
---
+++ cadastroProdutos/src/components/cadastroProduto.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+    msg: String,
+});
+
+const nomeProduto = ref('');
+const categoria = ref('');
+const dataCadastro = ref('');
+const funcionarios = ref(false);
+
+const cadastroFuncionario = () => {
+    funcionarios.value = !mostrarTemplateAdicional.value;
+};
+
+</script>
+<template>
+    <div class="card">
+        <h1>{{ msg }}</h1>
+        <form @submit.prevent>
+            <div class="form-group">
+                <label for="nomeProduto">Nome do Produto:</label>
+                <input type="text" id="nomeProduto" v-model="nomeProduto" />
+            </div>
+            <div class="form-group">
+                <label for="categoria">Categoria:</label>
+                <input type="text" id="categoria" v-model="categoria" />
+            </div>
+            <div class="form-group">
+                <label for="dataCadastro">Data do Cadastro:</label>
+                <input type="date" id="dataCadastro" v-model="dataCadastro" />
+            </div>
+            <button type="submit">Cadastrar</button>
+        </form>
+
+        <button @click="cadastroFuncionario" style="margin-top: 30px;">
+            {{ funcionarios ? 'Fechar cadastro de Funcionários' : 'Abrir cadastro de Funcionários' }}
+        </button>
+
+        <div v-if="funcionarios" class="cadastro-funcionarios">
+            
+            
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .card {
+        width: 900px;
+        height: 400px;
+        background-color: #3c3c3c;
+        padding: 20px; 
+        color: white; 
+        border-radius: 10px; 
+        border: 2px solid black;
+    }
+    .form-group {
+        margin-bottom: 15px;
+        & label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        & input {
+            width: 100%;
+            padding: 8px;
+            box-sizing: border-box; 
+    }
+    & button {
+        margin-top: 15px;
+        
+    }
+}
+
+.cadastro-funcionarios {
+    margin-top: 20px;
+    padding: 15px;
+    border: 1px dashed #ccc;
+    background-color: #4a4a4a; /* Um pouco mais claro que o card para diferenciar */
+    border-radius: 5px;
+}
+</style>
